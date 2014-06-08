c5d40167da0e7955516f2d01518bfc0a535bc34f
dillonhua <dillonhua@gmail.com>
2014-06-08 11:37:52 +0800

    debug pwm ok,15000/1000

--- bsp/stm32f0x/drivers/timer.c
+++ bsp/stm32f0x/drivers/timer.c
@@ -13,50 +13,26 @@
  */
 
 #include <stm32f0xx.h>
-//#include <rtdevice.h>
 #include "timer.h"
-
-static void RCC_Configuration(void)
-{
-  NVIC_InitTypeDef NVIC_InitStructure;
-
-  /* TIM3 clock enable */
-  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
-
-  /* Enable the TIM3 gloabal Interrupt */
-  NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;
-  NVIC_InitStructure.NVIC_IRQChannelPriority = 0;
-  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-  NVIC_Init(&NVIC_InitStructure);
-
-}
+#define MAX_LEVEL 15000
+#define PLUS	1000
 void buzzer_ctl(int flag)
 {
-uint16_t capture = 0,x=0;
+  static uint16_t x=0;
   if(flag==1)
   {
-  TIM_Cmd(TIM3, ENABLE);
-  	if(x==700000)
-		x=0;
+	rt_kprintf("compare %d\r\n",TIM_GetCapture1(TIM3));
+	if(x==MAX_LEVEL)
+	  x=PLUS;
 	else
-		x=x+100000;
-  	capture = TIM_GetCapture1(TIM3);
+	  x=x+PLUS;
 	TIM_SetCompare1(TIM3, x);
-	rt_kprintf("on capture is %d\r\n",capture);
-	/* TIM Interrupts enable */
-	//TIM_ITConfig(TIM3, TIM_IT_CC1, ENABLE);
-	/* TIM2 enable counter */
-	
-	//TIM_CtrlPWMOutputs(TIM3, ENABLE);
+	TIM_Cmd(TIM3, ENABLE);
   }
   else
   {
-	/* TIM Interrupts enable */
-	//TIM_ITConfig(TIM3, TIM_IT_CC1, DISABLE);
-	/* TIM2 enable counter */
+	while(TIM_GetCounter(TIM3)!=0);
 	TIM_Cmd(TIM3, DISABLE);
-	//TIM_CtrlPWMOutputs(TIM1, DISABLE);
-	rt_kprintf("off\r\n");
   }
 
 }
@@ -65,7 +41,7 @@ void rt_hw_timer_init(void)
   TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
   TIM_OCInitTypeDef        TIM_OCInitStructure;
   GPIO_InitTypeDef GPIO_InitStructure;
-    
+
   RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
   GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
@@ -74,15 +50,14 @@ void rt_hw_timer_init(void)
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
   GPIO_Init(GPIOA, &GPIO_InitStructure);
   GPIO_PinAFConfig(GPIOA, GPIO_PinSource6, GPIO_AF_1);
-RCC_Configuration();
   RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
   /* TIM3 Configuration */
   TIM_DeInit(TIM3);
   TIM_TimeBaseStructInit(&TIM_TimeBaseStructure);
   TIM_OCStructInit(&TIM_OCInitStructure);  
   /* Time base configuration */
-  TIM_TimeBaseStructure.TIM_Period = 100000;
-  TIM_TimeBaseStructure.TIM_Prescaler = 0x0;       
+  TIM_TimeBaseStructure.TIM_Period = MAX_LEVEL;
+  TIM_TimeBaseStructure.TIM_Prescaler = 0;       
   TIM_TimeBaseStructure.TIM_ClockDivision = 0x0;    
   TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;  
   TIM_TimeBaseStructure.TIM_RepetitionCounter = 0;
@@ -91,34 +66,15 @@ RCC_Configuration();
   /* Output Compare PWM Mode configuration */
   TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM2;//TIM_OCMode_Timing;
   TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;       
-  TIM_OCInitStructure.TIM_OutputNState = TIM_OutputNState_Enable;
-  TIM_OCInitStructure.TIM_Pulse = 5000;
-  TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_Low;
+  TIM_OCInitStructure.TIM_OutputNState = TIM_OutputNState_Disable;
+  TIM_OCInitStructure.TIM_Pulse = PLUS;
+  TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
   TIM_OCInitStructure.TIM_OCNPolarity = TIM_OCNPolarity_High;
-  TIM_OCInitStructure.TIM_OCIdleState = TIM_OCIdleState_Set;
+  TIM_OCInitStructure.TIM_OCIdleState = TIM_OCIdleState_Reset;
   TIM_OCInitStructure.TIM_OCNIdleState = TIM_OCIdleState_Reset;
   TIM_OC1Init(TIM3, &TIM_OCInitStructure);
-  //TIM_SetAutoreload(TIM3,300000);
-  //TIM_ARRPreloadConfig(TIM3,ENABLE);
-  	TIM_Cmd(TIM3, ENABLE);
-	TIM_CtrlPWMOutputs(TIM3, ENABLE);
-
-}
-void TIM3_IRQHandler(void)
-{
-  uint16_t capture = 0;
-  rt_interrupt_enter();
-  if (TIM_GetITStatus(TIM3, TIM_IT_CC1) != RESET)
-  {
-	TIM_ClearITPendingBit(TIM3, TIM_IT_CC1);
-
-	
-	//read_batt();
-	capture = TIM_GetCapture1(TIM3);
-	TIM_SetCompare1(TIM3, capture + 1000);
-	rt_kprintf("capture is %d\r\n",capture);
-  }
-  rt_interrupt_leave();
+  TIM_Cmd(TIM3, ENABLE);
+  TIM_CtrlPWMOutputs(TIM3, ENABLE);
 }
 INIT_DEVICE_EXPORT(rt_hw_timer_init);
 
