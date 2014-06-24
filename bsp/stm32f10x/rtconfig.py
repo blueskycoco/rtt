12e61d02f461f46471f88de8cf58da5fbefa559e
dillon <dillon.min@irdeto.com>
2014-06-24 18:03:31 +0800

    debug version

--- bsp/stm32f10x/rtconfig.py
+++ bsp/stm32f10x/rtconfig.py
@@ -3,7 +3,7 @@ import os
 # toolchains options
 ARCH='arm'
 CPU='cortex-m3'
-CROSS_TOOL='keil'
+CROSS_TOOL='gcc'
 
 if os.getenv('RTT_CC'):
 	CROSS_TOOL = os.getenv('RTT_CC')
@@ -36,7 +36,7 @@ elif CROSS_TOOL == 'iar':
 if os.getenv('RTT_EXEC_PATH'):
 	EXEC_PATH = os.getenv('RTT_EXEC_PATH')
 
-BUILD = 'debug'
+BUILD = 'debug1'
 
 if PLATFORM == 'gcc':
     # toolchains
