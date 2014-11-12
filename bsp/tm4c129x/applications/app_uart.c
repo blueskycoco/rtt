81f7364dccb39a35127e9257d37c0a3b995d9bd0
dillon <dillon.min@irdeto.com>
2014-11-12 17:58:16 +0800

    update code

--- bsp/tm4c129x/applications/app_uart.c
+++ bsp/tm4c129x/applications/app_uart.c
@@ -36,7 +36,6 @@ int app_uart_init()
         &uart_thread_stack[0], sizeof(uart_thread_stack),
         20, 10);
 	g_lwip_app=(plwip_app)rt_malloc(sizeof(lwip_app));
-	/*write g_lwip_app register default val*/
 	
 	mutex = rt_mutex_create("mutex", RT_IPC_FLAG_FIFO);
 	if (mutex == RT_NULL)
@@ -44,6 +43,8 @@ int app_uart_init()
 	  rt_kprintf("create mutex failed\n");
 	  return 0;
 	}
+	/*write g_lwip_app register default val*/
+	set_default_reg();
 	if (result == RT_EOK)
 	  rt_thread_startup(&uart_thread);
     return 1;
@@ -55,28 +56,78 @@ rt_bool_t cs_low()
 void set_default_reg()
 {
 	rt_err_t result;
+	int i;
 	result = rt_mutex_take(mutex, RT_WAITING_FOREVER);
-	g_lwip_app->common->mr=0x00;
-	g_lwip_app->common->gar0=0x00;
-	g_lwip_app->common->gar1=0x00;
-	g_lwip_app->common->gar2=0x00;
-	g_lwip_app->common->gar3=0x00;
-	g_lwip_app->common->subr0=0x00;
-	g_lwip_app->common->subr1=0x00;
-	g_lwip_app->common->subr2=0x00;
-	g_lwip_app->common->subr3=0x00;	
-	g_lwip_app->common->shar0=0x00;
-	g_lwip_app->common->shar1=0x00;
-	g_lwip_app->common->shar2=0x00;
-	g_lwip_app->common->shar3=0x00;
-	g_lwip_app->common->shar4=0x00;
-	g_lwip_app->common->shar5=0x00;	
-	g_lwip_app->common->sipr0=0x00;
-	g_lwip_app->common->sipr1=0x00;
-	g_lwip_app->common->sipr2=0x00;
-	g_lwip_app->common->sipr3=0x00;
-	g_lwip_app->common->ir=0x00;
-	g_lwip_app->common->imr=0x00;
+	g_lwip_app->common->mr=		0x00;
+	g_lwip_app->common->gar0=	0x00;
+	g_lwip_app->common->gar1=	0x00;
+	g_lwip_app->common->gar2=	0x00;
+	g_lwip_app->common->gar3=	0x00;
+	g_lwip_app->common->subr0=	0x00;
+	g_lwip_app->common->subr1=	0x00;
+	g_lwip_app->common->subr2=	0x00;
+	g_lwip_app->common->subr3=	0x00;	
+	g_lwip_app->common->shar0=	0x00;
+	g_lwip_app->common->shar1=	0x00;
+	g_lwip_app->common->shar2=	0x00;
+	g_lwip_app->common->shar3=	0x00;
+	g_lwip_app->common->shar4=	0x00;
+	g_lwip_app->common->shar5=	0x00;	
+	g_lwip_app->common->sipr0=	0x00;
+	g_lwip_app->common->sipr1=	0x00;
+	g_lwip_app->common->sipr2=	0x00;
+	g_lwip_app->common->sipr3=	0x00;
+	g_lwip_app->common->ir=		0x00;
+	g_lwip_app->common->imr=	0x00;
+	g_lwip_app->common->rtr0=	0x07;
+	g_lwip_app->common->rtr1=	0xd0;
+	g_lwip_app->common->rcr=	0x08;
+	g_lwip_app->common->rmsr=	0x55;
+	g_lwip_app->common->tmsr=	0x55;	
+	g_lwip_app->common->uipr0=	0x00;
+	g_lwip_app->common->uipr1=	0x00;
+	g_lwip_app->common->uipr2=	0x00;
+	g_lwip_app->common->uipr3=	0x00;
+	g_lwip_app->common->uport0=	0x00;
+	g_lwip_app->common->uport1=	0x00;
+	for(i=0;i<4;i++)
+	{
+		g_lwip_app->socket[i]->mr=	0x00;
+		g_lwip_app->socket[i]->cr=	0x00;
+		g_lwip_app->socket[i]->ir=	0x00;
+		g_lwip_app->socket[i]->sr=	0x00;		
+		g_lwip_app->socket[i]->port0=	0x00;
+		g_lwip_app->socket[i]->port1=	0x00;
+		g_lwip_app->socket[i]->dhar0=	0x00;
+		g_lwip_app->socket[i]->dhar1=	0x00;
+		g_lwip_app->socket[i]->dhar2=	0x00;
+		g_lwip_app->socket[i]->dhar3=	0x00;
+		g_lwip_app->socket[i]->dhar4=	0x00;
+		g_lwip_app->socket[i]->dhar5=	0x00;
+		g_lwip_app->socket[i]->dipr0=	0x00;
+		g_lwip_app->socket[i]->dipr1=	0x00;
+		g_lwip_app->socket[i]->dipr2=	0x00;
+		g_lwip_app->socket[i]->dipr3=	0x00;
+		g_lwip_app->socket[i]->dport0=0x00;
+		g_lwip_app->socket[i]->dport1=0x00;
+		g_lwip_app->socket[i]->mssr0=	0xff;
+		g_lwip_app->socket[i]->mssr1=	0xff;
+		g_lwip_app->socket[i]->proto=	0x00;
+		g_lwip_app->socket[i]->tos=	0x00;
+		g_lwip_app->socket[i]->ttl=	0x80;
+		g_lwip_app->socket[i]->txfsr0=0x08;
+		g_lwip_app->socket[i]->txfsr1=0x00;
+		g_lwip_app->socket[i]->txrd0=	0x00;
+		g_lwip_app->socket[i]->txrd1=	0x00;
+		g_lwip_app->socket[i]->txwr0=	0x00;
+		g_lwip_app->socket[i]->txwr1=	0x00;
+		g_lwip_app->socket[i]->rxrsr0=0x00;
+		g_lwip_app->socket[i]->rxrsr1=0x00;
+		g_lwip_app->socket[i]->rxrd0=	0x00;
+		g_lwip_app->socket[i]->rxrd1=	0x00;
+		g_lwip_app->socket[i]->rxwr0=	0x00;
+		g_lwip_app->socket[i]->rxwr1=	0x00;
+	}
 	rt_mutex_release(mutex);
 }
 rt_uint8_t rw_reg(rt_uint16_t addr,rt_uint8_t data,rt_bool_t rw)
@@ -87,20 +138,27 @@ rt_uint8_t rw_reg(rt_uint16_t addr,rt_uint8_t data,rt_bool_t rw)
 	if(addr>=COMMON_REG_MR && addr<=COMMON_REG_TMSR)
 	{
 		if(rw)/*write*/
-			*(rt_uint8_t *)(g_lwip_app->common+addr)=data;
+		{
+			if(addr!=COMMON_REG_IR)
+				*(rt_uint8_t *)(g_lwip_app->common+addr)=data;
+		}
 		else
 			ret=*(rt_uint8_t *)(g_lwip_app->common+addr);
 	}
 	else if(addr>=COMMON_REG_UIPR0 && addr<=COMMON_REG_UPORT1)
 	{
 		if(rw)/*write*/
-			*(rt_uint8_t *)(g_lwip_app->common+addr-0x10)=data;
+		{
+			if(addr!=COMMON_REG_UIPR0&&addr!=COMMON_REG_UIPR1&&addr!=COMMON_REG_UIPR2&&addr!=COMMON_REG_UIPR3&&addr!=COMMON_REG_UPORT0&&addr!=COMMON_REG_UPORT1)
+				*(rt_uint8_t *)(g_lwip_app->common+addr-0x10)=data;
+		}
 		else
 			ret=*(rt_uint8_t *)(g_lwip_app->common+addr-0x10);
 	}
 	else if(addr>=SOCKET_REG_S0_MR && addr<=SOCKET_REG_S0_TTL)
 	{
 		if(rw)/*write*/
+			if(addr!=SOCKET_REG_S0_IR&&addr!=SOCKET_REG_S0_SR)
 			*(rt_uint8_t *)(g_lwip_app->socket[0]+addr)=data;
 		else
 			ret=*(rt_uint8_t *)(g_lwip_app->socket[0]+addr);
@@ -108,6 +166,8 @@ rt_uint8_t rw_reg(rt_uint16_t addr,rt_uint8_t data,rt_bool_t rw)
 	else if(addr>=SOCKET_REG_S0_TX_FSR0 && addr<=SOCKET_REG_S0_RX_WR1)
 	{
 		if(rw)/*write*/
+			if(addr!=SOCKET_REG_S0_TX_FSR0&&addr!=SOCKET_REG_S0_TX_FSR1&&addr!=SOCKET_REG_S0_TX_RD0&&addr!=SOCKET_REG_S0_TX_RD1&&\
+				addr!=SOCKET_REG_S0_RX_RSR0&&addr!=SOCKET_REG_S0_RX_RSR1)
 			*(rt_uint8_t *)(g_lwip_app->socket[0]+addr-0x40B)=data;
 		else
 			ret=*(rt_uint8_t *)(g_lwip_app->socket[0]+addr-0x40B);
@@ -115,6 +175,7 @@ rt_uint8_t rw_reg(rt_uint16_t addr,rt_uint8_t data,rt_bool_t rw)
 	else if(addr>=SOCKET_REG_S1_MR && addr<=SOCKET_REG_S1_TTL)
 	{
 		if(rw)/*write*/
+			if(addr!=SOCKET_REG_S1_IR&&addr!=SOCKET_REG_S1_SR)
 			*(rt_uint8_t *)(g_lwip_app->socket[1]+addr)=data;
 		else
 			ret=*(rt_uint8_t *)(g_lwip_app->socket[1]+addr);
@@ -122,6 +183,8 @@ rt_uint8_t rw_reg(rt_uint16_t addr,rt_uint8_t data,rt_bool_t rw)
 	else if(addr>=SOCKET_REG_S1_TX_FSR0 && addr<=SOCKET_REG_S1_RX_WR1)
 	{
 		if(rw)/*write*/
+			if(addr!=SOCKET_REG_S1_TX_FSR0&&addr!=SOCKET_REG_S1_TX_FSR1&&addr!=SOCKET_REG_S1_TX_RD0&&addr!=SOCKET_REG_S1_TX_RD1&&\
+				addr!=SOCKET_REG_S1_RX_RSR0&&addr!=SOCKET_REG_S1_RX_RSR1)
 			*(rt_uint8_t *)(g_lwip_app->socket[1]+addr-0x50B)=data;
 		else
 			ret=*(rt_uint8_t *)(g_lwip_app->socket[1]+addr-0x50B);
@@ -129,6 +192,7 @@ rt_uint8_t rw_reg(rt_uint16_t addr,rt_uint8_t data,rt_bool_t rw)
 	else if(addr>=SOCKET_REG_S2_MR && addr<=SOCKET_REG_S2_TTL)
 	{
 		if(rw)/*write*/
+			if(addr!=SOCKET_REG_S2_IR&&addr!=SOCKET_REG_S2_SR)
 			*(rt_uint8_t *)(g_lwip_app->socket[2]+addr)=data;
 		else
 			ret=*(rt_uint8_t *)(g_lwip_app->socket[2]+addr);
@@ -136,6 +200,8 @@ rt_uint8_t rw_reg(rt_uint16_t addr,rt_uint8_t data,rt_bool_t rw)
 	else if(addr>=SOCKET_REG_S2_TX_FSR0 && addr<=SOCKET_REG_S2_RX_WR1)
 	{
 		if(rw)/*write*/
+			if(addr!=SOCKET_REG_S2_TX_FSR0&&addr!=SOCKET_REG_S2_TX_FSR1&&addr!=SOCKET_REG_S2_TX_RD0&&addr!=SOCKET_REG_S2_TX_RD1&&\
+				addr!=SOCKET_REG_S2_RX_RSR0&&addr!=SOCKET_REG_S2_RX_RSR1)
 			*(rt_uint8_t *)(g_lwip_app->socket[2]+addr-0x60B)=data;
 		else
 			ret=*(rt_uint8_t *)(g_lwip_app->socket[2]+addr-0x60B);
@@ -143,6 +209,7 @@ rt_uint8_t rw_reg(rt_uint16_t addr,rt_uint8_t data,rt_bool_t rw)
 	else if(addr>=SOCKET_REG_S3_MR && addr<=SOCKET_REG_S3_TTL)
 	{
 		if(rw)/*write*/
+			if(addr!=SOCKET_REG_S3_IR&&addr!=SOCKET_REG_S3_SR)
 			*(rt_uint8_t *)(g_lwip_app->socket[3]+addr)=data;
 		else
 			ret=*(rt_uint8_t *)(g_lwip_app->socket[3]+addr);
@@ -150,6 +217,8 @@ rt_uint8_t rw_reg(rt_uint16_t addr,rt_uint8_t data,rt_bool_t rw)
 	else if(addr>=SOCKET_REG_S3_TX_FSR0 && addr<=SOCKET_REG_S3_RX_WR1)
 	{
 		if(rw)/*write*/
+			if(addr!=SOCKET_REG_S3_TX_FSR0&&addr!=SOCKET_REG_S3_TX_FSR1&&addr!=SOCKET_REG_S3_TX_RD0&&addr!=SOCKET_REG_S3_TX_RD1&&\
+				addr!=SOCKET_REG_S3_RX_RSR0&&addr!=SOCKET_REG_S3_RX_RSR1)
 			*(rt_uint8_t *)(g_lwip_app->socket[3]+addr-0x70B)=data;
 		else
 			ret=*(rt_uint8_t *)(g_lwip_app->socket[3]+addr-0x70B);
