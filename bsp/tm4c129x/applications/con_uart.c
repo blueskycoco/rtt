f32ae7c8c06f27966bc8ec9e063feb925838b8ae
b550 <b550@gmail.com>
2014-11-15 11:29:52 +0800

    new solution update

--- bsp/tm4c129x/applications/con_uart.c
+++ bsp/tm4c129x/applications/con_uart.c
@@ -6,9 +6,9 @@ static struct rt_thread uart_thread;
 ALIGN(RT_ALIGN_SIZE)
 static char uart_thread_stack[2048];
 /*send config data or socket data*/
-int uart_rw_config()
+int uart_rw_config(unsigned char ch)
 {
-
+	
 }
 /*get config data to global config zone, or get socket data to buffer*/
 int uart_rw_socket()
@@ -19,13 +19,22 @@ rt_bool_t ind_low()
 {
 	return ((MAP_GPIOPinRead(GPIO_PORTN_BASE, GPIO_PIN_0)&(0x1<<GPIO_PIN_7))==0)?RT_TRUE:RT_FALSE);
 }
+enum STATE_OP{
+	GET_F5,
+	GET_8A_8B,
+	GET_DATA,
+	GET_26,
+	GET_FA
+};
 
 void uart_thread_entry(void* parameter)
 {
 	char ch,rw=0;/*0 is r , 1 is w*/
 	   rt_uint16_t addr=0x0000;
 	   rt_uint8_t send_reg;
-	   STATE_OP state=SLEEP;
+	   rt_uint8_t buf[6];
+	   rt_uint8_t i=0,len=0,param;
+	   STATE_OP state=GET_F5;
 	   while (1)
 	   {
 		 /* wait receive */
@@ -42,7 +51,128 @@ void uart_thread_entry(void* parameter)
 			/*config data parser*/
 			while((rt_device_read(uart_dev, 0, &ch, 1) == 1))
 			{
-				uart_rw_config(ch);
+				switch(state)
+				{
+					case GET_F5:
+						if(ch==0xf5)
+							state=GET_8A_8B;
+						break;
+					case GET_8A_8B:
+						if(ch==0x8a)
+							state=GET_DATA;
+						else if(ch==0x8b)
+						{
+							/*send config data out*/
+							unsigned char i,*ptr=(unsigned char *)g_conf;
+							int result=0;
+							get_config[0]=0xf5;
+							get_config[1]=0x8C;
+							result=0xf5+0x8c+0x27+0xfa;
+							for(i=0;i<sizeof(g_conf);i++)
+							{
+								get_config[2+i]=ptr[i];
+								result=result+get_config[2+i];
+							}
+							get_config[2+i+1]=0x27;
+							get_config[2+i+2]=0xfa;
+							get_config[2+i+3]=(result&0xff00)>>8;
+							get_config[2+i+4]=result&0xff;
+							rt_device_write(uart_dev,0,get_config,sizeof(get_config));
+						}
+							state=GET_F5;
+						break;
+					case GET_DATA:
+						{
+							param=ch;
+							if(ch==0||ch==2||ch==3||ch==5||ch==6||ch==7||ch==8||ch==13||ch==14||ch==15)
+								len=4;
+							else if(ch==1||ch==9||ch==10||ch==11||ch==12)
+								len=2;
+							else if(ch==4)
+								len=6;
+							for(i=0;i<len;i++)
+								while((rt_device_read(uart_dev,0,&(buf[i]),1)==0));
+							state=GET_26;
+						}
+					break;
+					case GET_26:
+						if(ch==0x26)
+							state=GET_FA;
+						else
+							state=GET_F5;
+						break;
+					case GET_FA:
+						if(ch==0xFA)
+						{
+							unsigned char crc1,crc2,*ptr;
+							int result=0xf5+0x8a+param+0x26+0xfa;
+							while((rt_device_read(uart_dev,0,&(crc1),1)==0));
+							while((rt_device_read(uart_dev,0,&(crc2),1)==0));
+							for(i=0;i<len;i++)
+								result=result+buf[i];
+							if(result==(crc1<<8|crc2))
+							{
+								switch(param)
+								{
+									case 0://local ip		
+									ptr=g_conf.local_ip;
+									break;
+									case 1://local port		
+									ptr=g_conf.local_port;
+									break;
+									case 2://sub msk		
+									ptr=g_conf.sub_msk;
+									break;
+									case 3://gw ip		
+									ptr=g_conf.gw;
+									break;
+									case 4://mac		
+									ptr=g_conf.mac;
+									break;
+									case 5://socket 0 ip		
+									ptr=g_conf.remote_ip0;
+									break;
+									case 6://socket 1 ip		
+									ptr=g_conf.remote_ip1;
+									break;
+									case 7://socket 2 ip		
+									ptr=g_conf.remote_ip2;
+									break;
+									case 8://socket 3 ip			
+									ptr=g_conf.remote_ip3;
+									break;
+									case 9://socket 0 port			
+									ptr=g_conf.remote_port0;
+									break;
+									case 10://socket 1 port	
+									ptr=g_conf.remote_port1;
+									break;
+									case 11://socket 2 port	
+									ptr=g_conf.remote_port2;
+									break;
+									case 12://socket 3 port		
+									ptr=g_conf.remote_port3;
+									break;									
+									case 13://protol
+									ptr=g_conf.protol;
+									break;
+									case 14://server or client mode
+									ptr=g_conf.server_mode;
+									break;									
+									case 15://socket uart baud
+									ptr=g_conf.uart_baud;
+									break;
+									default:
+										ptr=NULL;
+								}
+								if(ptr!=NULL)
+								for(i=0;i<len;i++)
+									ptr[i]=buf[i];
+							}
+						}
+							state=GET_F5;
+							break;
+				}
 			}
 		}
 	   }
