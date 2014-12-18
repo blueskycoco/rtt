8c9ebaea985fee0be486a0d4cde346adb9684230
U-CORP\dillon.min <dillon.min@CNBNG-LT542.Corp.irdeto.com>
2014-12-18 15:31:43 +0800

    update branch stm32f030_demo

--- bsp/stm32f0x/drivers/cmx865a.c
+++ bsp/stm32f0x/drivers/cmx865a.c
@@ -1,6 +1,5 @@
 #include <stm32f0xx.h>
 #include "cmx865a.h"
-#define HW_SPI 0
 SPI_InitTypeDef  SPI_InitStructure;
 rt_uint8_t send_data=0;
 rt_uint8_t recv_data=0;
@@ -59,22 +58,11 @@ void init_spi()
 	NVIC_InitTypeDef NVIC_InitStructure;
 	init_irq();
 	
-	#if HW_SPI
-	/* Enable the SPI periph */
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
-	#endif
 	/* Enable SCK, MOSI, MISO and NSS GPIO clocks */
 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA|RCC_AHBPeriph_GPIOF, ENABLE);
 
-	//GPIO_PinAFConfig(GPIOA, GPIO_PinSource5, GPIO_AF_0);//sck
-	//GPIO_PinAFConfig(GPIOA, GPIO_PinSource6, GPIO_AF_0);//miso
-	//GPIO_PinAFConfig(GPIOA, GPIO_PinSource7, GPIO_AF_0);//mosi
-	//GPIO_PinAFConfig(GPIOA, GPIO_PinSource4, GPIO_AF_0);//cs
-	#if HW_SPI
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-	#else
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-	#endif
+
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_DOWN;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_Level_3;
@@ -88,9 +76,6 @@ void init_spi()
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 
 	/* SPI MISO pin configuration */
-	#if !HW_SPI
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
-	#endif
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
@@ -98,44 +83,9 @@ void init_spi()
 
 	/* SPI NSS pin configuration */
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
-	#if HW_SPI
-	/* SPI configuration -------------------------------------------------------*/
-	SPI_I2S_DeInit(SPI1);
-	SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
-	SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;
-	SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low;
-	SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge;
-	SPI_InitStructure.SPI_NSS = SPI_NSS_Hard;
-	SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_8;
-	SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
-	SPI_InitStructure.SPI_CRCPolynomial = 7;
-
-	/* Configure the SPI interrupt priority */
-	NVIC_InitStructure.NVIC_IRQChannel = SPI1_IRQn;
-	NVIC_InitStructure.NVIC_IRQChannelPriority = 1;
-	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-	NVIC_Init(&NVIC_InitStructure);
-
-	/* Initializes the SPI communication */
-	SPI_InitStructure.SPI_Mode = SPI_Mode_Master;
-	SPI_Init(SPI1, &SPI_InitStructure);
-
-	/* Initialize the FIFO threshold */
-	SPI_RxFIFOThresholdConfig(SPI1, SPI_RxFIFOThreshold_QF);
-
-	/* Enable the Rx buffer not empty interrupt */
-	SPI_I2S_ITConfig(SPI1, SPI_I2S_IT_RXNE, ENABLE);
-	/* Enable the SPI Error interrupt */
-	SPI_I2S_ITConfig(SPI1, SPI_I2S_IT_ERR, ENABLE);
-	/* Data transfer is performed in the SPI interrupt routine */
-	SPI_SSOutputCmd(SPI1,ENABLE);
-	//SPI_NSSPulseModeCmd(SPI1,ENABLE);
-	/* Enable the SPI peripheral */
-	SPI_Cmd(SPI1, ENABLE);
-	#endif
 }
 void ms_delay()
 {
@@ -145,15 +95,7 @@ void ms_delay()
 }
 rt_uint8_t write_spi(rt_uint8_t data)
 {
-	#if HW_SPI
-	send_data=data;
-	/* Enable the Tx buffer empty interrupt */
-	SPI_I2S_ITConfig(SPI1, SPI_I2S_IT_TXE, ENABLE);
-
-	/* Waiting until TX FIFO is empty */
-	while (SPI_GetTransmissionFIFOStatus(SPI1) != SPI_TransmissionFIFOStatus_Empty)
-	{}
-	#else
+	
 	rt_uint8_t i; 
 	rt_uint8_t Temp=0x00;
 	unsigned char SDI; 
@@ -184,29 +126,7 @@ rt_uint8_t write_spi(rt_uint8_t data)
 	}
 	//GPIO_SetBits(GPIOA, GPIO_Pin_4);
 	return Temp; //返回读到miso输入的值     
-
-
-          
-	#endif
 }
-#if HW_SPI
-
-rt_uint8_t read_spi()
-{
-	
-	/* Wait busy flag */
-	while(SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_BSY) == SET)
-	{}
-
-	/* Waiting until RX FIFO is empty */
-	while (SPI_GetReceptionFIFOStatus(SPI1) != SPI_ReceptionFIFOStatus_Empty)
-	{}
-
-	
-	return recv_data;
-}
-#endif
-
 void write_cmx865a(rt_uint8_t addr,rt_uint16_t data,rt_uint8_t len)
 {
 	GPIO_ResetBits(GPIOA, GPIO_Pin_4);
@@ -236,33 +156,6 @@ void read_cmx865a(rt_uint8_t addr,rt_uint8_t* data,rt_uint8_t len)
 		}
 	GPIO_SetBits(GPIOA, GPIO_Pin_4);
 }
-#if HW_SPI
-void SPI1_IRQHandler(void)
-{
-
-  /* SPI in Master Tramitter mode--------------------------------------- */
-  if (SPI_I2S_GetITStatus(SPI1, SPI_I2S_IT_TXE) == SET)
-  {
-    
-      SPI_SendData8(SPI1, send_data);
-      SPI_I2S_ITConfig(SPI1, SPI_I2S_IT_TXE, DISABLE);
-     
-  }
-  
-  /* SPI in Master Receiver mode--------------------------------------- */
-  if (SPI_I2S_GetITStatus(SPI1, SPI_I2S_IT_RXNE) == SET)
-  {
-    recv_data=SPI_ReceiveData8(SPI1);
-  }
-  
-  /* SPI Error interrupt--------------------------------------- */
-  if (SPI_I2S_GetITStatus(SPI1, SPI_I2S_IT_OVR) == SET)
-  {
-    SPI_ReceiveData8(SPI1);
-    SPI_I2S_GetITStatus(SPI1, SPI_I2S_IT_OVR);
-  }
-}
-#endif
 void button_isr(void)
 {
 	/*rt_kprintf("button_isr intr\r\n");
@@ -283,9 +176,7 @@ void cmx865a_isr(void)
 	static unsigned char  k=0; 
 	static unsigned char  fsk_long=0; 
 	read_cmx865a(Status_addr,&i,2);
-	//rt_kprintf("cmx865a_isr intr %x\r\n",i);
-	//if(GPIO_ReadInputDataBit(GPIOF,GPIO_Pin_1)==Bit_RESET)
-	//GPIO_SetBits(GPIOA, GPIO_Pin_9);
+	
 	if(DTMF_MODE)
 	{
 		if(i&0x0020)//??DTMF
