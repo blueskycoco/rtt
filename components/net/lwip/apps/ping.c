6684620a0db7d329af7a24009a7a77a8b1c99040
U-CORP\dillon.min <dillon.min@CNBNG-LT542.Corp.irdeto.com>
2014-12-30 15:51:31 +0800

    ping is ok

--- components/net/lwip/apps/ping.c
+++ components/net/lwip/apps/ping.c
@@ -59,8 +59,8 @@ static void ping_prepare_echo( struct icmp_echo_hdr *iecho, u16_t len)
     {
         ((char*)iecho)[sizeof(struct icmp_echo_hdr) + i] = (char)i;
     }
-
-    iecho->chksum = inet_chksum(iecho, len);
+	/*remove chksum ,tm4c129x can hw set chksum*/
+    //iecho->chksum = inet_chksum(iecho, len);
 }
 
 /* Ping using the socket ip */
