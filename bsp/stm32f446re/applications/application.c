5e68863ca6955c4b35a641ed757a964aafef0ce4
U-CORP\dillon.min <dillon.min@CNBNG-LT542.Corp.irdeto.com>
2015-09-24 11:27:28 +0800

    add stm32f446re

---
+++ bsp/stm32f446re/applications/application.c
@@ -0,0 +1,42 @@
+/*
+ * File      : application.c
+ * This file is part of RT-Thread RTOS
+ * COPYRIGHT (C) 2006, RT-Thread Development Team
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rt-thread.org/license/LICENSE
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2009-01-05     Bernard      the first version
+ * 2014-04-27     Bernard      make code cleanup. 
+ */
+
+#include <board.h>
+#include <rtthread.h>
+
+void rt_init_thread_entry(void* parameter)
+{
+	while(1)
+	{
+		
+		rt_thread_delay(100);
+	}
+}
+
+int rt_application_init()
+{
+    rt_thread_t tid;
+
+    tid = rt_thread_create("init",
+        rt_init_thread_entry, RT_NULL,
+        2048, RT_THREAD_PRIORITY_MAX/3, 20);
+
+    if (tid != RT_NULL)
+        rt_thread_startup(tid);
+
+    return 0;
+}
+
+/*@}*/
