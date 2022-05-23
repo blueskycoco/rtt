7a7acc6bad70ec4fbcd6da4819f127eba94b41be
Dillon Min <dillon.minfei@gmail.com>
2022-05-23 18:11:44 +0800

    add bootloader feature
    - add ota
    - add jump
    
    Signed-off-by: Dillon Min <dillon.minfei@gmail.com>

--- bsp/stm32/stm32f767-st-nucleo/applications/main.c
+++ bsp/stm32/stm32f767-st-nucleo/applications/main.c
@@ -13,6 +13,7 @@
 #include <rtdevice.h>
 #include <board.h>
 #include <fal.h>
+#include <stdbool.h>
 
 /* defined the LED1 pin: PB0 */
 #define LED1_PIN    GET_PIN(B, 0)
@@ -21,9 +22,10 @@
 #include "md5.h"
 
 typedef  void (*jump_app)(void);
-#define APP_ADDRESS 0x08040000	//app
-#define APP_LEN_ADDR 0x08020000	//param
-#define APP_MD5_ADDR 0x08020004
+#define APP_ADDRESS 	0x08040000	//app
+#define APP_LEN_ADDR 	0x08020000	//param
+#define APP_MD5_ADDR 	0x08100004
+#define OTA_ADDRESS		0x08100000
 
 jump_app jump;
 
@@ -44,40 +46,84 @@ static void app_boot()
 	}
 }
 
-void verify_and_jump()
+bool ota()
 {
-	uint8_t *ptr = (uint8_t *)APP_LEN_ADDR;
-	uint32_t fw_len = (ptr[0] << 24) | (ptr[1] << 16) | (ptr[2] << 8) | (ptr[3]);
-	uint8_t fw_md5[16];
-	int i;
-	uint8_t boot = 1;
+	/* verify ota zone */
 	MD5_CTX md5; 
-	uint8_t decrypt[16] = {0};    
-
-	rt_memcpy((void *)fw_md5,
-			(const void *)(__IO uint8_t *)APP_MD5_ADDR, 16);
-
-	rt_kprintf("fw len: %u %x\r\n", fw_len, fw_len);
-
-	//if (fw_len > 256*1024)
-	//	return;
-	fw_len = 512 * 1024;
-	ptr = (uint8_t *)APP_ADDRESS;
+	uint8_t decrypt[16] = {0};
+	uint32_t i;
+	uint8_t *ptr = (uint8_t *)(OTA_ADDRESS);
+	uint32_t ota_len = (ptr[0] << 24) | (ptr[1] << 16) | (ptr[2] << 8) | (ptr[3]);
+	uint8_t *md5_sum = (uint8_t *)(OTA_ADDRESS + 4);
+	rt_kprintf("app len: %d\n", ota_len);
 	MD5Init(&md5);
-	MD5Update(&md5, ptr, fw_len);
+	MD5Update(&md5, ptr + 20, ota_len);
 	MD5Final(&md5, decrypt);
-
 	for (i = 0; i < 16; i++) {
-		if (decrypt[i] == fw_md5[i])
-			rt_kprintf("%02x == %02x\r\n", decrypt[i], fw_md5[i]);
-		else {
-			rt_kprintf("%02x != %02x\r\n", decrypt[i], fw_md5[i]);
-			boot = 0;
-		}
+		rt_kprintf("md5[%d] %02x %02x\n", i, decrypt[i], md5_sum[i]);
+		if (decrypt[i] != md5_sum[i])
+			return false;
+	}
+	/* compare ota zone with app zone */
+	ptr = (uint8_t *)(APP_ADDRESS);
+	MD5Init(&md5);
+	MD5Update(&md5, ptr, ota_len);
+	MD5Final(&md5, decrypt);
+	for (i = 0; i < 16; i++) {
+		rt_kprintf("app md5[%d] %02x %02x\n", i, decrypt[i], md5_sum[i]);
+		if (decrypt[i] != md5_sum[i])
+			break;
 	}
 
-	//if (boot == 1)
-		app_boot();
+	/* cp ota zone to app zone as they are different */
+	if (i != 16) {
+    	static const struct fal_partition *part_dev = NULL;
+		if ((part_dev = fal_partition_find("app")) == NULL) {
+			rt_kprintf("can't find app zone");
+			return false;
+		}
+		ptr = (uint8_t *)(OTA_ADDRESS + 20);
+		for (i = 0; i < ota_len/262144; i++) {
+			ptr += i*262144;
+			if (fal_partition_erase(part_dev, i*262144, 256*1024) < 0) {
+				rt_kprintf("erase app zone failed %d", i*262144);
+				return false;
+			}
+			if (fal_partition_write(part_dev, i*262144,
+						(const uint8_t *)ptr, 256 * 1024) < 0) {
+				rt_kprintf("write to app zone failed %d", i*262144);
+				return false;
+			}
+		}
+
+		if (ota_len % 262144) {
+			ptr += i*262144;
+			if (fal_partition_erase(part_dev, i*262144, 256*1024) < 0) {
+				rt_kprintf("erase app zone failed %d", i);
+				return false;
+			}
+			if (fal_partition_write(part_dev, i*262144,
+						(const uint8_t *)ptr, ota_len - i*262144) < 0) {
+				rt_kprintf("write to app zone failed %d", ota_len - i*262144);
+				return false;
+			}
+		}
+		/* recheck app zone */	
+		ptr = (uint8_t *)(APP_ADDRESS);
+		MD5Init(&md5);
+		MD5Update(&md5, ptr, ota_len);
+		MD5Final(&md5, decrypt);
+		for (i = 0; i < 16; i++) {
+			rt_kprintf("recheck app md5[%d] %02x %02x\n", i, decrypt[i], md5_sum[i]);
+			if (decrypt[i] != md5_sum[i])
+				return false;
+		}
+
+		return true;
+	} else
+		rt_kprintf("no need ota\n");
+
+	return false;
 }
 #endif
 
@@ -87,7 +133,9 @@ int main(void)
     rt_pin_mode(LED1_PIN, PIN_MODE_OUTPUT);
 	fal_init();
 #ifndef BSP_WORKING_AS_APP
-	verify_and_jump();
+	if (ota())
+		rt_kprintf("ota success\n");
+	app_boot();
 #else
 	rt_kprintf("hello 123\n");
 #endif
